11127717eff0bfe24031bfedd5daed89867c3938
Surbhi1976 <surbhikumari1345@gmail.com>
2025-11-14 19:42:13 +0530

    Add Increment Submatrices by One solution
    Implement a solution for incrementing submatrices based on given queries.

---
+++ Daily LeetCode Questions/Increment Submatrices by One.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>>mat(n,vector<int>(n,0));
+
+        for(auto &query : queries){
+            int r1=query[0];
+            int r2=query[2];
+            int c1=query[1];
+            int c2=query[3];
+            for(int i=r1;i<=r2;i++){
+                mat[i][c1]+=1;
+                if(c2+1<n){
+                mat[i][c2+1]-=1;}
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                mat[i][j]+=mat[i][j-1];
+            }
+        }
+        return mat;
+    }
+};
